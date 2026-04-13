bee920cacff26770dff1af33e4f85ce20895d9f0
Nursima Asilturk <nursima.asilturk99@gmail.com>
2026-04-14 01:48:53 +0300

    login and update functions are created in service

--- src/e-commerce/user/user.controller.ts
+++ src/e-commerce/user/user.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  createUser(@Body() body:any){
-    return this.userService.createUser(body);
+  createUser(@Body() dto:CreateUserDto){
+    return this.userService.createUser(dto);
   }
   
   @Get()
@@ -17,6 +20,15 @@ export class UserController {
 
   @Get(':id')
   getUserById(@Param('id') id:string){
-    return this.userService.getUserById(Number(id));
+    return this.userService.getUserById(+id);
   }
+  @Patch(':id')
+  update(@Param('id') id:string,@Body() dto:UpdateUserDto){
+    return this.userService.updateUser(+id,dto)
+  }
+  @Post('login')
+  login(@Body() dto:LoginUserDto){
+    return this.userService.login(dto);
+  }
+
 }
--- src/e-commerce/user/user.service.ts
+++ src/e-commerce/user/user.service.ts
@@ -1,20 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { dot } from 'node:test/reporters';
 
 @Injectable()
 export class UserService {
 	constructor(private prisma:PrismaService){}
-	createUser(data){
+	createUser(data:CreateUserDto){
 		return this.prisma.user.create({data});
 	}
 	getAllUsers(){
 		return this.prisma.user.findMany();
 	}
-	getUserById(id){
+	getUserById(id:number){
 		return this.prisma.user.findUnique({
 			where:{
 				id
 			}
 		})
+	}
+	updateUser(id:number,data:UpdateUserDto){
+		return this.prisma.user.update({
+			where:{
+				id
+			},
+			data:data
+		})
+	}
+	async login(data:LoginUserDto){
+		const user = await this.prisma.user.findUnique({
+			where:{
+				email:data.email
+			}
+		});
+		if(!user){
+			throw new UnauthorizedException('Invalid credentials!');
+		}
+		if(user.password !== data.password){
+			throw new UnauthorizedException('Invalid credentials!');
+		}
+		const {password,...result} = user;
+		return result; 
 	}
 }
